7fdfca926b2796bf793eaffdc7679894eab584ad
Matheus Dias <dias.math0@gmail.com>
2023-02-12 12:36:47 -0300

    Commit 5

--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ dotenv.config({ path: './.env.development' });
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send(`My name is ${process.env.NAME}`);
+});
+
 app.listen(port, () => {
     console.log(`Running on: http://localhost:${port}`);
 });
